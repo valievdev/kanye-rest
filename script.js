a58376dc4a3530cce289af13adf7061dc4e51b4a
azat valiev <azvaliev@gmail.com>
2022-04-16 07:29:02 -0700

    clarified instructions

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // API URL 
-/// https://developer.mozilla.org/en-US/docs/Web/API/fetch
 const API_URL = "https://api.kanye.rest";
 alert('hello world');
 async function main() {
+	/// https://developer.mozilla.org/en-US/docs/Web/API/fetch
+	// Use fetch api 
 	const data = await 	//command goes here
 }
